5743e6ec1c3064df348180d1d58b4b14a04343d4
ol1c <155087424+ol1c@users.noreply.github.com>
2025-03-09 22:23:36 +0100

    lab2 zad5

---
+++ lab2/zadanie5/page_rank.m
@@ -0,0 +1,18 @@
+function [index_number, Edges, I, B, A, b, r] = page_rank()
+index_number = 197808;
+Edges = [1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 1, 3, 4, 5, 6, 7;
+         4, 6, 3, 4, 5, 5, 6, 7, 5, 6, 4, 6, 4, 7, 6, 2, 2, 2, 2, 2, 2];
+I = speye(7);
+B = sparse(Edges(2,:), Edges(1,:), 1, 7, 7);
+A = spdiags(sum(B, 1)' .^ (-1), 0, 7, 7);
+b = (1 - 0.85) / 7 * ones(7, 1);
+M = I - 0.85 * B * A;
+r = M \ b;
+
+bar(r);
+hold on;
+title("PageRank");
+xlabel("Strony");
+ylabel("Współczynnik PR dla stron");
+hold off;
+end

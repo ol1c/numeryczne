678c44c9c26b4b19f123dc0f8eeef79f75b40106
ol1c <155087424+ol1c@users.noreply.github.com>
2025-03-19 23:27:34 +0100

    buy-sell examples

--- projekt1_macd/display_func.py
+++ projekt1_macd/display_func.py
@@ -1,12 +1,25 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-def plot_settings():
+def plot_settings(day_interval):
     plt.figure(figsize=(12, 7))
     plt.subplots_adjust(bottom=0.15, top=0.95)
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=30))
-    plt.xticks(rotation=90)
-def plot_data(data, buy, sell):
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=day_interval))
+    plt.xticks(rotation=45)
+def plot_macd(data, buy, sell, title, day_interval):
+    buy_data, buy_value, buy_signal = zip(*buy)
+    sell_data, sell_value, sell_signal = zip(*sell)
+
+    plot_settings(day_interval)
+    plt.plot(data['Date'], data['MACD'], color='blue', label='MACD')
+    plt.plot(data['Date'], data['SIGNAL'], color='red', label='SIGNAL')
+    plt.scatter(buy_data, buy_signal, color='green', label='Zakup', zorder=5)
+    plt.scatter(sell_data, sell_signal, color='pink', label='Sprzedaż', zorder=5)
+    plt.legend()
+    plt.title(title)
+    plt.show()
+
+def plot_value(data, buy, sell, title, day_interval):
     buy_data, buy_value, buy_signal = zip(*buy)
     sell_data, sell_value, sell_signal = zip(*sell)
 
@@ -15,17 +28,10 @@ def plot_data(data, buy, sell):
     # plt.legend()
     # plt.show()
 
-    plot_settings()
-    plt.plot(data['Date'], data['MACD'], color='blue', label='MACD')
-    plt.plot(data['Date'], data['SIGNAL'], color='red', label='SIGNAL')
-    plt.scatter(buy_data, buy_signal, color='green', label='Zakup', zorder=5)
-    plt.scatter(sell_data, sell_signal, color='pink', label='Sprzedaż', zorder=5)
-    plt.legend()
-    plt.show()
-
-    plot_settings()
+    plot_settings(day_interval)
     plt.plot(data['Date'], data['Value'], color='k', label='Wartości akcji')
     plt.scatter(buy_data, buy_value, color='green', label='Zakup', zorder=5)
     plt.scatter(sell_data, sell_value, color='red', label='Sprzedaż', zorder=5)
     plt.legend()
+    plt.title(title)
     plt.show()
--- projekt1_macd/main.py
+++ projekt1_macd/main.py
@@ -4,10 +4,24 @@ from data import *
 from macd_func import *
 from display_func import *
 
+def example_buy_sell(data, buy, sell, i):
+    example_buy = [buy[i]]
+    example_sell = [sell[i+3]]
+    start_date = example_buy[0][0]
+    end_date = example_sell[0][0]
+    mask = (data['Date'] >= start_date) & (data['Date'] <= end_date)
+    filtered_data = data[mask]
+    profit = example_sell[0][1] - example_buy[0][1]
+    plot_macd(filtered_data, example_buy, example_sell, "Profit: " + str(profit), 7)
+
 
 data = get_data()
 data['MACD'] = get_macd(data['Value'])
 data['SIGNAL'] = get_signal(data['MACD'])
 buy, sell = find_buy_and_sell_points(data)
 
-plot_data(data, buy, sell)
+plot_macd(data, buy, sell, "", 30)
+plot_value(data, buy, sell, "", 30)
+
+example_buy_sell(data, buy, sell, 1)
+example_buy_sell(data, buy, sell, 10)

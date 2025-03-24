c7148fe0c1ddde5a044f64d8b22f193db91252e5
ol1c <155087424+ol1c@users.noreply.github.com>
2025-03-24 20:39:10 +0100

    lab3 zad3

---
+++ lab3/zadanie3/solve_Jacobi.m
@@ -0,0 +1,43 @@
+function [A,b,M,w,x,r_norm,iteration_count] = solve_Jacobi()
+% A - macierz z równania macierzowego A * x = b
+% b - wektor prawej strony równania macierzowego A * x = b
+% M - macierz pomocnicza opisana w instrukcji do Laboratorium 3
+%       – sprawdź wzór (5) w instrukcji, który definiuje M jako M_J.
+% w - wektor pomocniczy opisany w instrukcji do Laboratorium 3
+%       – sprawdź wzór (5) w instrukcji, który definiuje w jako w_J.
+% x - rozwiązanie równania macierzowego wyznaczone metodą Jacobiego
+% iteration_count - liczba iteracji wykonanych przy wyznaczeniu rozwiązania
+% r_norm - wektor norm residuum kolejnych przybliżeń rozwiązania:
+%       - r_norm(1) = norm(A*ones(N,1)-b)
+%       – r_norm(i) = norm(A*x-b), gdzie x stanowi i-te przybliżenie rozwiązania
+%       - length(r_norm) = iteration_count + 1
+
+N = 6000;
+
+[A,b] = generate_matrix(N);
+L = tril(A, -1);
+U = triu(A, 1);
+D = diag(diag(A));
+
+iteration_count = 0;
+
+M = -D \ (L + U);
+w = D \ b;
+x = ones(N,1);
+r_norm = [];
+
+
+inorm = norm(A*x-b);
+r_norm = inorm;
+while(inorm>1e-12 && iteration_count<1000)
+    %x = M*x + w;
+    x = M * x + w;
+    inorm = norm(A*x-b);
+    iteration_count = iteration_count+1;
+    r_norm = [ r_norm, inorm ];
+end
+semilogy(0:iteration_count, r_norm);
+title('Norma residuum dla kolejnych iteracji');
+xlabel('Iteracje');
+ylabel('Norma residuum');
+end

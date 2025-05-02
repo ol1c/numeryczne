f4e539adfa78c7ba3b21a3c19773933df0f68e9c
ol1c <155087424+ol1c@users.noreply.github.com>
2025-05-02 23:43:08 +0200

    benchmarks (E)

--- projekt2_alu/main.py
+++ projekt2_alu/main.py
@@ -13,6 +13,28 @@ def plotSemilogy(x, y, title):
     plt.ylabel("Norma residuum")
     plt.show()
 
+def plotTime(x, time1, time2, time3):
+    plt.figure(figsize=(12, 7))
+    plt.subplots_adjust(bottom=0.15, top=0.95)
+    plt.subplot(2, 1, 1)
+    plt.plot(x, time1, label="metoda Jacobiego")
+    plt.plot(x, time2, label="metoda Gaussa-Seidla")
+    plt.plot(x, time3, label="metoda rozkładu LU")
+    plt.legend()
+    plt.xlabel("rozmiar macieży N")
+    plt.ylabel("czas potrzebny do obliczenia [ns]")
+    plt.title("Porównanie czasu potzrebnego do rozwiązania równania")
+    plt.subplot(2, 1, 2)
+    plt.semilogy(x, time1, label="metoda Jacobiego")
+    plt.semilogy(x, time2, label="metoda Gaussa-Seidla")
+    plt.semilogy(x, time3, label="metoda rozkładu LU")
+    plt.legend()
+    plt.xlabel("rozmiar macieży N")
+    plt.ylabel("czas potrzebny do obliczenia [ns]")
+    plt.title("Porównanie czasu potzrebnego do rozwiązania równania (skala log)")
+    plt.tight_layout()
+    plt.show()
+
 
 def LUD(A):
     return np.tril(A, k=-1),  np.triu(A, k=1), np.diag(np.diag(A))
@@ -86,45 +108,68 @@ def LUMethod(A, b):
     directTime = time.perf_counter_ns() - start
     return x, np.linalg.norm(A @ x - b), factorizationTime + directTime
 
-# zad A
-N = 1208
-A = np.zeros([N, N])
-np.fill_diagonal(A, 13)
-np.fill_diagonal(A[1:], -1)
-np.fill_diagonal(A[:,1:], -1)
-np.fill_diagonal(A[2:], -1)
-np.fill_diagonal(A[:,2:], -1)
-b = np.sin([n * 8 for n in range(N)])
+# # zad A
+# N = 1208
+# A = np.zeros([N, N])
+# np.fill_diagonal(A, 13)
+# np.fill_diagonal(A[1:], -1)
+# np.fill_diagonal(A[:,1:], -1)
+# np.fill_diagonal(A[2:], -1)
+# np.fill_diagonal(A[:,2:], -1)
+# b = np.sin([n * 8 for n in range(N)])
+#
+# # zad B
+# x, iterations, rNorm, calculationTime = JacobiMethod(A, b)
+# print("\nMetoda Jacobiego")
+# print("Iteracje potrzebne do rozwiązania:", iterations)
+# print("Czas potrzebny do rozwiązania:", calculationTime, "ns")
+# plotSemilogy([i for i in range(iterations+1)], rNorm, "Zmiana normy residuum dla metody Jacobiego")
+#
+# x, iterations, rNorm, calculationTime = GaussSeidlMethod(A, b)
+# print("\nMetoda Gaussa-Seidla")
+# print("Iteracje potrzebne do rozwiązania:", iterations)
+# print("Czas potrzebny do rozwiązania:", calculationTime, "ns")
+# plotSemilogy([i for i in range(iterations+1)], rNorm, "Zmiana normy residuum dla metody Gaussa-Seidla")
+#
+# # zad C
+# np.fill_diagonal(A, 3)
+# x, iterations, rNorm, calculationTime = JacobiMethod(A, b)
+# print("\nMetoda Jacobiego")
+# print("Iteracje potrzebne do rozwiązania:", iterations)
+# print("Czas potrzebny do rozwiązania:", calculationTime, "ns")
+# plotSemilogy([i for i in range(iterations+1)], rNorm, "Zmiana normy residuum dla metody Jacobiego")
+#
+# x, iterations, rNorm, calculationTime = GaussSeidlMethod(A, b)
+# print("\nMetoda Gaussa-Seidla")
+# print("Iteracje potrzebne do rozwiązania:", iterations)
+# print("Czas potrzebny do rozwiązania:", calculationTime, "ns")
+# plotSemilogy([i for i in range(iterations+1)], rNorm, "Zmiana normy residuum dla metody Gaussa-Seidla")
+#
+# # zad D
+# x, rNorm, calculationTime = LUMethod(A, b)
+# print("\nMetoda rozkładu LU")
+# print("Czas potrzebny do rozwiązania:", calculationTime, "ns")
+# print("Residuum norm:", rNorm)
 
-# zad B
-x, iterations, rNorm, calculationTime = JacobiMethod(A, b)
-print("\nMetoda Jacobiego")
-print("Iteracje potrzebne do rozwiązania:", iterations)
-print("Czas potrzebny do rozwiązania:", calculationTime, "ns")
-plotSemilogy([i for i in range(iterations+1)], rNorm, "Zmiana normy residuum dla metody Jacobiego")
+# zad E
+size = [100, 500, 1000, 2000, 3000, 4000, 5000]
+JacobiTime = []
+GaussSeidlTime = []
+LUTime = []
+for N in size:
+    print(N)
+    A = np.zeros([N, N])
+    np.fill_diagonal(A, 13)
+    np.fill_diagonal(A[1:], -1)
+    np.fill_diagonal(A[:, 1:], -1)
+    np.fill_diagonal(A[2:], -1)
+    np.fill_diagonal(A[:, 2:], -1)
+    b = np.sin([n * 8 for n in range(N)])
+    x, iterations, rNorm, calculationTime = JacobiMethod(A, b)
+    JacobiTime.append(calculationTime*1e-9)
+    x, iterations, rNorm, calculationTime = GaussSeidlMethod(A, b)
+    GaussSeidlTime.append(calculationTime*1e-9)
+    x, rNorm, calculationTime = LUMethod(A, b)
+    LUTime.append(calculationTime*1e-9)
+plotTime(size, JacobiTime, GaussSeidlTime, LUTime)
 
-x, iterations, rNorm, calculationTime = GaussSeidlMethod(A, b)
-print("\nMetoda Gaussa-Seidla")
-print("Iteracje potrzebne do rozwiązania:", iterations)
-print("Czas potrzebny do rozwiązania:", calculationTime, "ns")
-plotSemilogy([i for i in range(iterations+1)], rNorm, "Zmiana normy residuum dla metody Gaussa-Seidla")
-
-# zad C
-np.fill_diagonal(A, 3)
-x, iterations, rNorm, calculationTime = JacobiMethod(A, b)
-print("\nMetoda Jacobiego")
-print("Iteracje potrzebne do rozwiązania:", iterations)
-print("Czas potrzebny do rozwiązania:", calculationTime, "ns")
-plotSemilogy([i for i in range(iterations+1)], rNorm, "Zmiana normy residuum dla metody Jacobiego")
-
-x, iterations, rNorm, calculationTime = GaussSeidlMethod(A, b)
-print("\nMetoda Gaussa-Seidla")
-print("Iteracje potrzebne do rozwiązania:", iterations)
-print("Czas potrzebny do rozwiązania:", calculationTime, "ns")
-plotSemilogy([i for i in range(iterations+1)], rNorm, "Zmiana normy residuum dla metody Gaussa-Seidla")
-
-# zad D
-x, rNorm, calculationTime = LUMethod(A, b)
-print("\nMetoda rozkładu LU")
-print("Czas potrzebny do rozwiązania:", calculationTime, "ns")
-print("Residuum norm:", rNorm)
